79d26643e70ea36f20443e9dfece070b2904c84c
uuuu1313 <a51903153@gmail.com>
2023-05-30 19:19:59 +0900

    관리자 페이지 오류 수정

--- src/main/java/com/itsue/entities/Configs.java
+++ src/main/java/com/itsue/entities/Configs.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity @Data
+@Builder
+@AllArgsConstructor @NoArgsConstructor
 public class Configs {
 
     @Id
